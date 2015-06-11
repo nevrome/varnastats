d22e37fb097b76bc4d4c3fc6514aa16050afa30d
nevrome <clemens@nevrome.de>
2015-06-11 23:47:46 +0200

    delrc is preliminary ready. Probably there are still some special input matrizes, that will cause problems.

--- R/delrc.R
+++ R/delrc.R
@@ -1,12 +1,11 @@
 #' Delete rows and columns of a data.frame by amount of values > 0 
 #'
 #' A data.frame with numeric values often contains rows and columns with an insufficient 
-#' amount of values > 0 for a certain task. For example Correspondence Analysis or 
-#' Bivariate Correlation Analysis requires a minimum amount of usable values. 
+#' amount of values > 0 for a certain task. For example correspondence analysis or 
+#' bivariate correlation analysis requires a minimum amount of usable values. 
 #' In an archaeological context this could apply for example, if certain find categories
-#' are particularly rare in burial site context.
+#' are particularly rare in a burial site context.
 #' \code{delrc} allows to remove rows and columns, that don't fulfill the requirements.
-#' It's an extension of the idea of \code{varnastats::delempty()}
 #'
 #' @param matrix data.frame with numeric values
 #' @param climit numeric value: How many values > 0 have to be present to consider a 
@@ -21,32 +20,51 @@
 #' 
 #' default = 0 (no row will be deleted)
 #' 
-#' @return data.frame without removed rows/colums
-#'
-#' @examples
-#' testmatrix <- data.frame(c1 = c(0,3,8,2), c2 = c(0,6,7,8), c3 = c(0,0,0,0))
+#' @return data.frame without removed rows/colums. 
 #' 
-#' delrc(testmatrix, climit = 3, rlimit = 1)
-#'  
+#' If no values or just one row are left in the data.frame after application of the selection 
+#' criteria, the output is FALSE.
+#' 
+#' @examples
+#' testmatrix <- data.frame(
+#' c1 = c(0,3,8,2), 
+#' c2 = c(0,6,7,0), 
+#' c3 = c(0,0,0,0),
+#' c4 = c(0,3,8,2), 
+#' c5 = c(0,6,7,0), 
+#' c6 = c(1,0,0,1)
+#' )
+#' 
+#' # The following code removes every column with less than 3 values > 0.
+#' # That will remove the columns c2, c3, c5 and c6.
+#' # Further, every row with less than 2 values gets removed. 
+#' # That will delete row 1.
+#' delrc(testmatrix, climit = 3, rlimit = 2)
+#' 
+#' testresult <- delrc(testmatrix, 3, 2)
+#' rowSums(booleanize(testresult))
+#' colSums(booleanize(testresult))
+#' 
 #' @export
 #' 
 
 delrc <- function(matrix, climit = 0, rlimit = 0) {
   
-  if (climit > length(matrix[1,])) {
-    climit <- length(matrix[1,])
+  # reduce too big input to the extends of the matrix to cover the possible maximum 
+  if (climit > ncol(matrix)) {
+    climit <- ncol(matrix) + 1
+  }
+  if (rlimit > nrow(matrix)) {
+    rlimit <- nrow(matrix) + 1
   }
   
-  if (rlimit > length(matrix[,1])) {
-    rlimit <- length(matrix[,1])
-  }
-  
-  # search empty columns and save index in a vector 
+  # nested function: search empty columns and save index in a vector 
   cdel <- function(matrix, climit) {
     n <- c()
     p <- 1
-    if (!(length(matrix[,1]) == 0 | length(matrix[,1]) == 0)) {
-      for (s in 1:length(matrix[1,])) {
+    if (!(ncol(matrix) == 0 | nrow(matrix) == 0) &&
+        !is.vector(matrix)) {
+      for (s in 1:ncol(matrix)) {
         if (length(which(matrix[,s] > 0)) < climit) {
           n[p] <- s
           p <- p + 1
@@ -56,13 +74,14 @@ delrc <- function(matrix, climit = 0, rlimit = 0) {
     return(n)
   }
   
-  # search empty rows and save index in a vector
+  # nested function: search empty rows and save index in a vector
   rdel <- function(matrix, rlimit) {
     z <- c()
     i <- 1
     u <- 1
-    if (!(length(matrix[,1]) == 0 | length(matrix[,1]) == 0)) {
-      for (i in 1:length(matrix[,1])) {
+    if (!(ncol(matrix) == 0 | nrow(matrix) == 0) &&
+        !is.vector(matrix)) {
+      for (i in 1:nrow(matrix)) {
         if (length(which(matrix[i,] > 0)) < rlimit) {
           z[u] <- i
           u <- u + 1
@@ -72,29 +91,39 @@ delrc <- function(matrix, climit = 0, rlimit = 0) {
     return(z)
   }
 
-  while (length(matrix[,1]) > 0 && 
-         length(matrix[1,]) > 0 && 
+  # loop to go through the matrix again and again until every relevant row/column is removed.
+  # That's necessary, because the removal of e.g. one column  could cause the need to remove an other
+  # row previously still relevant
+  while (nrow(matrix) > 0 && 
+         ncol(matrix) > 0 && 
          (!is.null(cdel(matrix, climit)) ||
          !is.null(rdel(matrix, rlimit)))
          ) {
     
+    # call search function
     n <- cdel(matrix, climit)
-    z <- rdel(matrix, rlimit)
     
-    # delete colums
-    if (!is.null(n) && length(matrix[1,]) >= max(n))  {
+    # delete columns
+    if (!is.null(n) && ncol(matrix) >= max(n))  {
       matrix <- matrix[,-n]
     } 
     
+    # call search function
+    z <- rdel(matrix, rlimit)
+    
     # delete rows
-    if (!is.null(z) && length(matrix[,1]) >= max(z)) {
+    if (!is.null(z) && nrow(matrix) >= max(z)) {
       matrix <- matrix[-z,]
     } 
-    
   }
   
-  return(matrix)
-  
- 
+  # nice and clean output: if no content is left, the function returns FALSE 
+  if (is.vector(matrix)) {
+    return(FALSE)
+  } else if (nrow(matrix) == 0 || ncol(matrix) == 0) {
+    return(FALSE)
+  } else {
+    return(matrix) 
+  }
 
 }
